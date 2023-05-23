6d53a4475456781b85a73ab5df593f9151fe08de
Poklip <qpokusnik@gmail.com>
2023-05-23 15:03:58 +0300

    New background for no city. Arzamas were added.

--- app/src/main/java/com/example/winfo/cities.kt
+++ app/src/main/java/com/example/winfo/cities.kt
@@ -2,7 +2,7 @@ package com.example.winfo
 
 import android.widget.ImageView
 
-val cities = arrayOf("Moscow", "Pretoria", "London", "Mumbai", "Cairo", "Zelenograd")
+val cities = arrayOf("Moscow", "Pretoria", "London", "Mumbai", "Cairo", "Zelenograd", "Arzamas")
 var currentCity: String = ""
 
 fun setCityImage(city: String, image: ImageView) {
@@ -13,5 +13,6 @@ fun setCityImage(city: String, image: ImageView) {
         "Mumbai" -> image.setImageResource(R.drawable.mumbai)
         "Cairo" -> image.setImageResource(R.drawable.cairo)
         "Zelenograd" -> image.setImageResource(R.drawable.zelenograd)
+        "Arzamas" -> image.setImageResource(R.drawable.arzamas)
     }
 }
